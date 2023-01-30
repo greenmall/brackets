bf818a1862ac33a24b72fe0e32aed40696c3c55c
greenmall <nyaszelenzelenzelen@gmail.com>
2023-01-30 06:11:00 +0200

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-  check('()', [['(', ')']]) // -> true
-check('((()))()', [['(', ')']]) // -> true
-check('())(', [['(', ')']]) // -> false
-check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]) // -> true
-check('[(])', [['(', ')'], ['[', ']']]) // -> false
-check('[]()', [['(', ')'], ['[', ']']]) // -> true
-check('[]()(', [['(', ')'], ['[', ']']]) // -> false
-
-// special case: opening and closing bracket can be the same :)
-
-check('||', [['|', '|']]) // -> true
-check('|()|', [['(', ')'], ['|', '|']]) // -> true
-check('|(|)', [['(', ')'], ['|', '|']]) // -> false
-check('|()|(||)||', [['(', ')'], ['|', '|']]) // -> true
+  let str1;
+  let bc = bracketsConfig.flat().join('').match(/.{2}/g);
+  for (let i = 0; i < bc.length; i++) {
+    str1 = str;
+    str = str.replaceAll(bc[i], '');
+    if (str1 !== str) i=-1;
+    if (str.length === 0) return true;
+  }
+  return false;
 }
+
